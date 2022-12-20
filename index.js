af131f88656ec6b3d8676c32b21eedbd9e2e3bd3
davidsalomon <me@davidsalomon.dev>
2022-12-19 23:27:02 -0600

    add connection to database

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 import express from "express";
 import { config } from "dotenv";
 import morgan from "morgan";
-
+import colors from "colors";
+import { connectDB } from "./config/database.js";
 // Route files
 import { router as bootcamps } from "./routes/bootcamps.js";
 
 // Load env vars
 config({ path: "./config/config.env" });
+
+// Connect to database
+connectDB();
 
 const app = express();
 
@@ -20,7 +24,15 @@ app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
 	PORT,
-	console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`)
+	console.log(
+		`Server running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold
+	)
 );
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (error, promise) => {
+	console.log(`Error: ${error.message}`.red);
+	server.close(() => process.exit(1));
+});
